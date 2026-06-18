9164797858d27cd084b6b69be15717b6c8169bc6
Eduardo Moraes <199427299+edu-moraess@users.noreply.github.com>
2026-06-18 13:33:45 -0300

    Criar src/session.py

---
+++ src/session.py
@@ -0,0 +1,16 @@
+import streamlit as st
+from src.load_data import load_and_preprocess_data
+from src.config import DATA_PATH
+
+
+def init_session():
+    """
+    Inicializa dataset global do app de forma segura
+    (evita ModuleNotFoundError e session_state quebrado)
+    """
+
+    if "df" not in st.session_state:
+        df = load_and_preprocess_data(DATA_PATH)
+        st.session_state.df = df
+
+    return st.session_state.df
